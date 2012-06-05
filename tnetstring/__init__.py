e207259bc6e8613682d3fd615404f519482ddc3f
Ryan Kelly <ryan@rfk.id.au>
2012-06-05 13:21:54 +1000

    Prep version 0.2.1 release.

--- tnetstring/__init__.py
+++ tnetstring/__init__.py
@@ -50,7 +50,7 @@ to translate python's unicode strings to and from binary blobs:
 
 __ver_major__ = 0
 __ver_minor__ = 2
-__ver_patch__ = 0
+__ver_patch__ = 1
 __ver_sub__ = ""
 __version__ = "%d.%d.%d%s" % (__ver_major__,__ver_minor__,__ver_patch__,__ver_sub__)
 
